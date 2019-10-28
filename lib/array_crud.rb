ed0ed6f95c46b3de127450fcc8a7d406a0cca06e
Henry Ly <henryly213@gmail.com>
2019-10-28 11:35:01 -0500

    intail commit

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,41 @@
 def create_an_empty_array
-  
+  array = []
+  array
 end
 
 def create_an_array
-  
+  array = [1,2,3,4]
+  array
 end
 
 def add_element_to_end_of_array(array, element)
   
+  array.push(element)
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ 
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+
+  array.push(1)
+  array
 end
 
 def remove_element_from_start_of_array(array)
   
+  array.pop(1)
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = []
+  array.push(1)
+  array
 end
 
 def retrieve_first_element_from_array(array)
